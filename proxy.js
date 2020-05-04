407d91d9fe964ad92d598d0de3c059b8a94b1e06
Sam Nydahl <sam.nydahl@smb.nu>
2020-05-04 21:12:01 +0200

    Added support for async handlers.

--- proxy.js
+++ proxy.js
@@ -2,26 +2,21 @@ const serializeError = require('serialize-error');
 const path = require('path');
 
 function handler(event, context, callback) {
-
   const [targetHandlerFile, targetHandlerFunction] = event.targetHandler.split(".");
-  const target = require(path.resolve(__dirname, '../..', targetHandlerFile));
+  const target = require(path.resolve(__dirname, '../..', event.location, targetHandlerFile));
 
-  target[targetHandlerFunction](event.body, context, (error, response) => {
-
-    if (error) {
-        // Return Serverless error to AWS sdk
-        callback(null, {
-            StatusCode: 500,
-            FunctionError: 'Handled',
-            Payload: serializeError(error)
-        })
-    } else {
-        // Return lambda function response to AWS SDK & pass through args from serverless.
-        callback(null, response)
-    }
-  });
-
-
+  target[targetHandlerFunction](event.body, context)
+    .then((response) => {
+      // Return lambda function response to AWS SDK & pass through args from serverless.
+      callback(null, response)
+    })
+    .catch((err) => {
+      // Return Serverless error to AWS sdk
+      callback(null, {
+        FunctionError: 'Unhandled',
+        ...serializeError(err)
+      })
+    })
 }
 
 module.exports.handler = handler;
